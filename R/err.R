1dd1869b85927a77c6cb7085f02d2341da40f400
guy <guy.joseph@10xgenomics.com>
2026-04-21 15:39:27 -0700

    fix: Error Message Handling
    WIP

--- R/err.R
+++ R/err.R
@@ -15,8 +15,8 @@ SUCCESS <- list(success = TRUE, msg = NULL) # nolint
 #' @noRd
 validation_err <- function(msg, name) {
   sprintf(
-    paste(
-      "\nIt looks like the formatting of your %s does not match the required formatting for LoupeR.",
+    paste0(
+      "\nIt looks like the formatting of your %s does not match the required formatting for LoupeR. ",
       "For further information, please see the documentation: 10xgen.com/louper\n\n%s"
     ),
     name,
@@ -28,9 +28,9 @@ validation_err <- function(msg, name) {
 #' @noRd
 general_err <- function(msg, name) {
   sprintf(
-    paste(
-      "\nIt looks like there was an issue with %s. For further information,",
-      "please see the documentation: 10xgen.com/louper\n\n%s",
+    paste0(
+      "\nIt looks like there was an issue with %s. ",
+      "For further information, please see the documentation: 10xgen.com/louper\n\n%s"
     ),
     name,
     msg
